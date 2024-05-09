05550996edc079fd8325555de5dfb4ccb91e4e22
dbusteed <busteed.davis@gmail.com>
2024-05-09 14:59:30 +0000

    Deploying to gh-pages from @ dbusteed/chicken-run@09a13935723cd8283ac53de0a9abf4841f4f73c4 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1715002353|4131953';
+const CACHE_VERSION = '1715194266|4806948';
 /** @type {string} */
 const CACHE_PREFIX = 'Chicken Run-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
